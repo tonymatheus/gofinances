dcb5dd48895b22be1b5d8801306b3d4f7c65f664
tonymatheus <tony.chaves14@gmail.com>
2022-03-01 08:45:17 -0300

    Start project with expo bar WorkFlow and add styled components

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { Dashboard } from "./src/screens/Dashboard";
+
+export default function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
