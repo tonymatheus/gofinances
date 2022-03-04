7d08874fc261f8eaa59db1bb0dbc0dab828bf449
tonymatheus <tony.chaves14@gmail.com>
2022-03-04 00:01:17 -0300

    Starts the header styles

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserInfo,
+  User,
+  Photo,
+  UserGreeting,
+  UserName,
+  UserWrapper,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/43850888?v=4",
+              }}
+            />
+            <User>
+              <UserGreeting>Hello</UserGreeting>
+              <UserName>Tony</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
