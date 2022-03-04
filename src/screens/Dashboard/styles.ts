845cf2c4667b0b629bfabbb506c32aad5eacd452
tonymatheus <tony.chaves14@gmail.com>
2022-03-03 22:45:11 -0300

    Apply new styles with the global styles

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components/native";
+import theme from "../../Global/styles/theme";
 
 export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-weight: bold;
   font-size: 20px;
-  color: red;
+  color: ${({ theme }) => theme.colors.title};
 `;
