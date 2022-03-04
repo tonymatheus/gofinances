d9348a1b110080b50a99f0b86a498386ccc0b1f5
tonymatheus <tony.chaves14@gmail.com>
2022-03-03 22:44:36 -0300

    Pass global styles to our application

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider } from "styled-components";
 
+import theme from "./src/Global/styles/theme";
 import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Dashboard />
-    </>
+    </ThemeProvider>
   );
 }
