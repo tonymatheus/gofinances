78c06e328d12c221e956b07547e8870a7f29e409
tonymatheus <tony.chaves14@gmail.com>
2022-04-13 07:58:56 -0300

    Implements Authentication hook and signScreen with a little bug

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, ReactNode, useContext } from "react";
+import * as AuthSession from "expo-auth-session";
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -13,6 +14,7 @@ interface User {
 
 interface IAuthContextData {
   user: User;
+  signInWithGoogle(): Promise<void>;
 }
 
 const AuthContext = createContext({} as IAuthContextData);
@@ -22,11 +24,28 @@ function AuthProvider({ children }: AuthProviderProps) {
     id: "1212",
     name: "Tony",
     email: "tony@teste.com",
-    photo: "teste",
   };
 
+  async function signInWithGoogle() {
+    try {
+      const CLIENT_ID =
+        "572156082185-99rhe16vere8a8qsq5g515qfe91sasoo.apps.googleusercontent.com";
+      const REDIRECT_URI = "https://auth.expo.io/@devton/gofinances";
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+      const response = await AuthSession.startAsync({ authUrl });
+
+      console.log("Resposta  de dados do user" + response);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+      {children}
+    </AuthContext.Provider>
   );
 }
 
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { Alert } from "react-native";
 import {
   Container,
   TitleWrapper,
@@ -16,8 +17,16 @@ import { SignInSocialButton } from "../../components/SignInSocialButton";
 import { useAuth } from "../../hooks/auth";
 
 export function SignIn() {
-  const { user } = useAuth();
-  console.log(user.name);
+  const { signInWithGoogle } = useAuth();
+
+  async function handleSignInWithGoogle() {
+    try {
+      return await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Náo foi poss[ivel estabelecer o login com a conta goole");
+    }
+  }
   return (
     <Container>
       <Header>
@@ -36,7 +45,11 @@ export function SignIn() {
       </Header>
       <Footer>
         <FooterWrapper>
-          <SignInSocialButton title="Acessar com Google" svg={GoogleSvg} />
+          <SignInSocialButton
+            title="Acessar com Google"
+            svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
+          />
           <SignInSocialButton title="Acessar Apple" svg={AppleSvg} />
         </FooterWrapper>
       </Footer>
